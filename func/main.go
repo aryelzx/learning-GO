c5fea905a5a3b7431e28b88f63cc23abf2732788
Aryel Ramos <aryel.goncalves@livsaude.com.br>
2024-05-14 08:41:15 -0300

    func variadicas

--- func/main.go
+++ func/main.go
@@ -9,6 +9,8 @@ valor, err := sum3(20, 10)
 	}
 
 	fmt.Println(valor) //se não encontra erro, printa o valor
+
+	fmt.Println(sum4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 }
 	
 func sum3(a, b int) (int, error) {
@@ -17,3 +19,13 @@ func sum3(a, b int) (int, error) {
 	}
 	return a + b, nil
 }
+
+// ===================== funções variádicas ============================
+func sum4(numeros ...int) int {
+	total := 0 
+	for _, numero := range numeros {
+		total += numero
+	}
+	return total
+}
+
