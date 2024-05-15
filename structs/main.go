c88f933e8703d2b963d547f947869edf3f485da2
Aryel Ramos <aryel.goncalves@livsaude.com.br>
2024-05-15 08:30:10 -0300

    composição de structs

--- structs/main.go
+++ structs/main.go
@@ -2,25 +2,40 @@ package main
 
 import "fmt"
 
+type Endereco struct {
+	Logradouro string
+	Bairro     string
+	Cidade     string
+	Estado     string
+}
+
 type Pessoa struct {
-    Nome     string
-    Idade    int
-    Profissao string
+	Nome      string
+	Idade     int
+	Profissao string
+	Ativo     bool
+	Adress    Endereco
 }
 
 func main() {
 	//criando uma instância da struct Pessoa
 	aryel := Pessoa{
-		Nome: "Aryel",
-		Idade: 21,
+		Nome:      "Aryel",
+		Idade:     21,
 		Profissao: "Desenvolvedor",
+		Ativo:     true,
 	}
+
+	aryel.Adress.Cidade = "Fortaleza"
 
 	//acessando os campos da struct
 	fmt.Println("Nome:", aryel.Nome)
 	fmt.Println("Idade: ", aryel.Idade)
 	fmt.Println("Profissão: ", aryel.Profissao)
+	fmt.Println("Ativo: ", aryel.Ativo)
 
 	aryel.Profissao = "Desenvolvedor FullStack"
 	fmt.Println("Nova Profissão:", aryel.Profissao)
+	//acessando o campo da struct Endereco
+	fmt.Println("Cidade:", aryel.Adress.Cidade)
 }
