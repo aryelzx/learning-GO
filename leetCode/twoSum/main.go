1290aa5c4a5aafcee9f0ae9bb402991460b149b5
aryelzx <arielcearamor102@gmail.com>
2024-05-30 17:28:26 -0300

    adding found indice

--- leetCode/twoSum/main.go
+++ leetCode/twoSum/main.go
@@ -6,34 +6,41 @@ package main
 import "fmt"
 
 func main() {
-	numsParams := []int{2, 7, 11, 15}
-	target := 9
+	numsParams := []int{3, 2, 4}
+	target := 6
 	a := twoSum(numsParams, target)
-	fmt.Println(a)
+	fmt.Println(a, "two sums result")
+	indices := findIndex(numsParams, a)
+	fmt.Println(indices, "indices")
 }
 
-var (
-	firstSum  int
-	secondSum int
-)
-
 func twoSum(nums []int, target int) []int {
-	arr := []int{}
+	arr := nums
 	result := []int{}
-
 	for i, _ := range nums {
-		arr = append(nums)
-		for j, _ := range nums {
+		for j, _ := range arr {
 			prox := i + 1
 			count := arr[j] + arr[prox]
+			firstSum := arr[j]
+			secondSum := arr[prox]
 			if count == target {
-				firstSum = arr[j]
-				secondSum = arr[prox]
-				fmt.Println(firstSum, secondSum)
-				// result = append(arr, arr[j], arr[prox])
+				result = append(result, firstSum, secondSum)
+				return result
 			}
+
 		}
-		return arr
 	}
 	return result
+}
+
+func findIndex(slice []int, value []int) []int {
+	indices := []int{}
+	for _, v := range slice {
+		for i, p := range value {
+			if p == v {
+				indices = append(indices, i)
+			}
+		}
+	}
+	return indices
 }
