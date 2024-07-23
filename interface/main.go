a19081cf970dfc714a34ff1209269bd3af1bea31
Aryel Ramos <aryel.goncalves@livsaude.com.br>
2024-07-23 11:40:30 -0300

    feat: type assertion

--- interface/main.go
+++ interface/main.go
@@ -12,6 +12,11 @@ func main() {
 
 	showType(x)
 	showType(y)
+
+	var minhaVar interface{} = "Aryel Ramos"
+	println(minhaVar.(string)) //type assertion
+	res, ok := minhaVar.(int)  //res - valor convertido para inteiro    ok - caso seja convertido com sucesso retorna true.
+	fmt.Printf(" O valor de res é %v e o resultado de ok é %v", res, ok)
 }
 
 func showType(t interface{}) {
